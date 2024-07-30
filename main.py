8fc4837f12b689422db895c500f53d44bdfd57bc
saqibtanveer786 <adward797@gmail.com>
2024-07-30 20:16:15 +0500

    initializing

---
+++ main.py
@@ -0,0 +1,45 @@
+import typer
+import os
+import time
+import shutil
+
+app = typer.Typer()
+
+directories = ['static', 'templates', 'models', 'views', 'forms']
+files = ['__init__.py', 'routes.py', 'config.py', 'run.py']
+
+def create_directories(parent_directory: str ,directories_list: list):
+    print("making directories")
+    with typer.progressbar(directories_list) as progress:
+        for dir in progress:
+            if not os.path.exists(f"./{parent_directory}/{dir}"):
+                os.mkdir(f"./{parent_directory}/{dir}")
+                progress.update(1)
+
+def create_files(parent_directory: str ,files_list: list):
+    print("making files")
+    with typer.progressbar(files_list) as progress:
+        for file in progress:
+            if not os.path.exists(f"./{parent_directory}/{file}"):
+                open(f"./{parent_directory}/{file}", 'w').close()
+                progress.update(1)
+                
+def delete_app(app_name):
+    if os.path.exists(app_name):
+        shutil.rmtree(app_name)
+
+@app.command()
+def create_app(name: str):
+    delete_app(name)
+    
+    if not os.path.exists(name):
+        os.makedirs(name)
+    
+
+    create_directories(name, directories)
+
+    create_files(name, files)
+
+
+if __name__ == "__main__":
+    app()
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A CLI to setup a flask app with just one command'
+LONG_DESCRIPTION = 'This is a CLI tool which setup a flask app with just one command. This is to ease devs. They just have to write setup-flask-app and every thing require to stat adding value to their app will be setup automatically.'
+
+# Setting up
+setup(
+    name="setup-flask-app",
+    version=VERSION,
+    author="Saqib Tanveer",
+    author_email="<elven5055@gmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    keywords=['python', 'flask', 'setup-flask-app', 'auto setup'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
