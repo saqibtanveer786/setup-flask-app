c8f27088de2f77fdf9f4eed4d37b2a0382403ddd
saqibtanveer786 <adward797@gmail.com>
2024-08-04 14:03:52 +0500

    wrote a basic test to check the structure

---
+++ tests/test_cli.py
@@ -0,0 +1,27 @@
+import unittest
+import os
+from pathlib import Path
+from typer.testing import CliRunner
+from setup_flask_app import app
+
+runner = CliRunner()
+
+
+DIRECTORIES = ['static', 'templates', 'models', 'views', 'forms']
+FILES = ['__init__.py', 'routes.py', 'config.py', 'run.py']
+TEST_DIR = 'test_project'
+
+class SimpleTest(unittest.TestCase):
+    def test_1_check_structure(self):
+        result = runner.invoke(app, [TEST_DIR])
+        self.assertTrue(result.exit_code == 0, "Cli command Failed")
+        for dir in DIRECTORIES:
+            self.assertTrue((Path.cwd()/TEST_DIR/dir).is_dir(), f"Directory {dir} is missing")
+
+        for file in FILES:
+            self.assertTrue((Path.cwd()/TEST_DIR/file).is_file(), f"File {file} is missing")
+            
+        print(f"test 1 passed directory structure is OK")
+
+    if __name__ == '__main__':
+        unittest.main()
