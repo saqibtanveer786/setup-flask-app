c59e7cb3adb7c1ee194893c6d89098004563e857
saqibtanveer786 <adward797@gmail.com>
2024-08-09 19:42:25 +0500

    moved the functions into another file called funcs.py

--- setup_flask_app/funcs.py
+++ setup_flask_app/funcs.py
@@ -1,13 +1,6 @@
 import typer
 import os
-from typing_extensions import Annotated
 import shutil
-from funcs import create_directories, create_files, copy_template_files, write_content_into_files
-
-app = typer.Typer()
-
-directories = ['static', 'templates']
-files = ['__init__.py']
 
 def create_directories(parent_directory: str ,directories_list: list):
     print("making directories")
@@ -56,34 +49,3 @@ def copy_template_files(parent_directory: str):
             for file in files:
                 shutil.copy(f"./templates/templates/{file}", f"{parent_directory}/templates")
                 continue
-            
-def delete_app(app_name):
-    if os.path.exists(app_name):
-        shutil.rmtree(app_name)
-
-@app.command()
-def create_app(name: str = typer.Argument(), boostrap: Annotated[bool, typer.Option(help="This is to setup boostrap along with app")] = False):
-    try:
-
-        if boostrap:
-            print("working")
-        
-        if not os.path.exists(name):
-            os.makedirs(name)
-        
-
-        create_directories(name, directories)
-
-        create_files(name, files)
-
-        write_content_into_files(name, files)
-
-        copy_template_files(f"{name}")
-
-    except Exception as e:
-        typer.echo(f"Error setting up app :  {e}")
-        typer.Exit(code=1)
-
-
-if __name__ == "__main__":
-    app()
